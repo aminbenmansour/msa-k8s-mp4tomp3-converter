7f3010ffcc111372579a594700c8d19d9685455e
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-11-24 23:17:28 +0100

    creating an auth service as a module and using it within microservice

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -2,6 +2,8 @@ import os, gridfs, pika
 from flask import Flask, request
 from flask_pymongo import PyMongo
 
+from auth_svc import access
+
 server = Flask(__name__)
 server.config["MONGO_URI"] = "mongodb://host.minikube.internal:27017/videos"
 
@@ -11,3 +13,7 @@ fs = gridfs.GridFS(mongo.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
