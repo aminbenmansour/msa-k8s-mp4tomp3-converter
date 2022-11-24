a58388587606de7cc665e129e1080e4366c099ae
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-11-25 00:46:35 +0100

    implementing the upload route

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -1,4 +1,4 @@
-import os, gridfs, pika
+import os, gridfs, pika, json
 from flask import Flask, request
 from flask_pymongo import PyMongo
 
@@ -27,3 +27,19 @@ def login():
 @server.route("/upload", methods=["POST"])
 def upload():
     access, err = validate.token(request)
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        if len(request.files) is not 1:
+            return "exactly 1 file required", 400
+
+        for _, f in request.files.items():
+            err = util.upload(f, fs, channel, access)
+
+            if err:
+                return err
+
+        return "success!", 200
+    else:
+        return "not authorized", 401
