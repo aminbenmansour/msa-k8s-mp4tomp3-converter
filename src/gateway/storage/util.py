4df3dcc11490c74e918542a3d7bd4ae53d576904
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-11-25 23:43:20 +0100

    implement basic upload (to be continued)

--- src/gateway/storage/util.py
+++ src/gateway/storage/util.py
@@ -2,4 +2,13 @@ import pika, json
 
 
 def upload(f, fs, channel, access):
-    pass
+    try:
+        fid = fs.put(f)
+    except Exception as err:
+        return "internal server error", 500
+    
+    message = {
+        "video_fid": str(fid),
+        "mp3_fid": None,
+        "username": access["username"],
+    }
