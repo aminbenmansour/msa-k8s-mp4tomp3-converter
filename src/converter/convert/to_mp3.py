d5d43b6e182d95a83a343e31aecc3c79c623cbae
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-01 15:22:28 +0100

    publishing msg to rabbitmq mp3 channel after finishing

--- src/converter/convert/to_mp3.py
+++ src/converter/convert/to_mp3.py
@@ -25,3 +25,18 @@ def start(message, fs_videos, fs_mp3s, channel):
     fid = fs_mp3s.put(data)
     f.close()
     os.remove(tf_path)
+
+    message["mp3_fid"] = str(fid)
+
+    try:
+        channel.basic_publish(
+            exchange="",
+            routing_key=os.environ.get("MP3_QUEUE"),
+            body=json.dumps(message),
+            properties=pika.BasicProperties(
+                delivery_mode=pika.spec.PERSISTANT_DELIVERY_MODE
+            ),
+        )
+    except Exception as err:
+        fs_mp3s.delete(fid)
+        return "failed to publish message"
