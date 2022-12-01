ef2b42330a0a27138fc3a3cc21c77d570eb858dc
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-01 12:59:06 +0100

    callback funcion on processing a msg from the queue

--- src/converter/consumer.py
+++ src/converter/consumer.py
@@ -16,6 +16,14 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     channel = connection.channel()
 
+    def callback(ch, method, properties, body):
+        err = to_mp3.start(body, fs_videos, fs_mp3s, ch)
+
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
     channel.basic_consume(
         queue=os.environ.get("VIDEO_QUEUE"), on_message_callback=callback
     )
