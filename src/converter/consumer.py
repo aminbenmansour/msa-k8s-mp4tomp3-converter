47e49c613f5a803eaca93779b6b18bd9bceb2072
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-03 16:53:34 +0100

    bug fixes (late cuz code cannot be tested yet)

--- src/converter/consumer.py
+++ src/converter/consumer.py
@@ -6,7 +6,7 @@ from convert import to_mp3
 def main():
     client = MongoClient("host.minikube.internal", 27017)
     db_videos = client.videos
-    db_mp3 = client.mp3
+    db_mp3s = client.mp3s
 
     # gridfs
     fs_videos = gridfs.GridFS(db_videos)
