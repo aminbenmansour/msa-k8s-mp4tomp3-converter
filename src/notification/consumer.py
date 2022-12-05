90a3591b3fba38275be69eb783de56aa19952590
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-05 20:55:11 +0100

    consuming service with rabbitmq

---
+++ src/notification/consumer.py
@@ -0,0 +1,30 @@
+import os, sys, time, pika
+from send import email
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        err = email.notification(body)
+
+        if err:
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(
+        queue=os.environ.get("MP3_QUEUE"), on_message_callback=callback
+    )
+
+    print("Waiting for messages. To exit press CTRL+C")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
